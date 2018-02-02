c81b6eea48eb16d46c2bea7d5cbd629ef46a8b1a
sharma0611 <sharma.shivam0611@gmail.com>
2018-02-01 22:06:57 -0500

    pulling basecoins

--- coincrunch.py
+++ coincrunch.py
@@ -2,6 +2,9 @@
 
 from lib.monitor import Monitor
 from lib.config import Config
+import ast
+import time
+from datetime import datetime
 
 max_latency = 5 
 strtime = "%Y-%m-%d %H:%M:%S"
@@ -12,8 +15,13 @@ quotecoin = 'USD'
 
 ### MAIN
 metadata = Config.get_variable("monitor", "metadata")
+metadata = ast.literal_eval(metadata)
 mc = Monitor(metadata)
 
 while True:
+    now = datetime.now()
     mc.update_data()
-    time.sleep(5)
+    end = datetime.now()
+    sleep_time = max(0, 5 - (end - now).total_seconds())
+    print(sleep_time)
+    time.sleep(sleep_time)
